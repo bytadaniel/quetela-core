87ee01fa4c69990de8061f545ec788cd0dc2301e
Daniel Byta <danielbyta.work@gmail.com>
2022-02-02 14:35:11 +0300

    rebuild

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/builtins'
-export * from './src/container'
-export * from './src/hooks'
-export * from './src/ignitor'
-export * from './src/models'
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './builtins'
+export * from './container'
+export * from './hooks'
+export * from './ignitor'
+export * from './models'
