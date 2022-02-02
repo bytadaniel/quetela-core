9db05555483bed3e5f12ae7bfde0fb766ec350bd
Daniel Byta <danielbyta.work@gmail.com>
2022-02-02 14:07:16 +0300

    ignitor fix

--- ignitor/Ignitor.ts
+++ ignitor/Ignitor.ts
@@ -4,15 +4,18 @@ import { TaskReference } from '../models'
 import { onProviderInit, onProviderReady, onProviderRegister } from '../hooks'
 
 export type IgnitorConfig = {
-  tasks: TaskReference[],
-  providers: ProviderReference[]
+  tasks?: TaskReference[],
+  providers?: ProviderReference[]
 }
 
 /**
  * Ignitor - это инициализатор приложения. Функция принимает ссылки на необходимые компоненты
  * после чего регистрирует все зависимости в Ioc контейнере и начинает прослушку задач
  */
-export async function Ignitor ({ providers, tasks }: IgnitorConfig): Promise<void> {
+export async function Ignitor ({
+  providers = [],
+  tasks = []
+}: IgnitorConfig): Promise<void> {
   const providerInstances = providers.map(Provider => new Provider(container))
   const taskInstances = tasks.map(Task => new Task())
 
