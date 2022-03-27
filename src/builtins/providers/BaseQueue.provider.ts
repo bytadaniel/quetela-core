f1f1272772134f17aed12b9a76f888a829d4b958
Daniel Byta <danielbyta.work@gmail.com>
2022-03-27 18:19:38 +0300

    package configuration

--- src/builtins/providers/BaseQueue.provider.ts
+++ src/builtins/providers/BaseQueue.provider.ts
@@ -1,7 +1,12 @@
+import { Ioc } from 'src/container/Ioc'
 import { Provider } from '../../models'
 import { NodeQueueClient } from '../queue-drivers/base-driver'
 
-export class NodeQueueProvider extends Provider {
+export class BaseQueueProvider extends Provider {
+  constructor(container: Ioc) {
+    super(container)
+  }
+
   public register () {
     this.container.bindSingleton<NodeQueueClient>('node-queue', () => new NodeQueueClient())
   }
